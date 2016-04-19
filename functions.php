78d8afcf97217d37a2ecf763d61ca8222f4a69d0
Ville Vuorenmaa <villevuor@gmail.com>
2016-04-19 09:34:05 +0300

    Basic page template

--- functions.php
+++ functions.php
@@ -11,17 +11,32 @@ function get_header() {
 			<meta name="description" content="">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 
-			<link rel="stylesheet" href="assets/css/normalize.min.css">
 			<link rel="stylesheet" href="assets/css/main.css">
 
 			<script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
 		</head>
 		<body>
+			<header id="site-header">
+
+				<a rel="home" href="/" title="Etusivu" class="site-title">Kisakanta</a>
+
+				<nav>
+					<ul>
+						<li><a href="/kilpailut">Kilpailut</a></li>
+						<li><a href="/tehtavat">Tehtävät</a></li>
+						<li><a href="/tietoa">Tietoa</a></li>
+					</ul>
+				</nav>
+
+			</header>
+
+			<main id="content" role="main">
 	<?php
 }
 
 function get_footer() {
 	?>
+			</main>
 
 			<script src="assets/js/main.js"></script>
 
@@ -39,7 +54,7 @@ function get_footer() {
 	<?php
 }
 
-function the_title() {
+function get_title() {
 	echo 'Kisakanta';
 }
 
