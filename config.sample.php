7104248609ae7bc5f54e9ec7deb2c2ebe6dd59d1
Ville Vuorenmaa <villevuor@gmail.com>
2016-10-16 14:58:24 +0300

    Git deployment

--- config.sample.php
+++ config.sample.php
@@ -10,4 +10,25 @@ $config['db']['password'] = '';
 $config['mailgun']['apikey'] = '';
 $config['mailgun']['domain'] = '';
 
+
+// Deployment configuration
+
+define('SECRET_ACCESS_TOKEN', 'BetterChangeMeNowOrSufferTheConsequences');
+define('REMOTE_REPOSITORY', 'https://github.com/villevuor/kisakanta.git');
+define('BRANCH', 'master');
+define('TARGET_DIR', '/');
+define('DELETE_FILES', false);
+define('EXCLUDE', serialize(array(
+	'.git',
+)));
+define('TMP_DIR', '/tmp/spgd-'.md5(REMOTE_REPOSITORY).'/');
+define('CLEAN_UP', false);
+define('VERSION_FILE', TMP_DIR.'VERSION');
+define('TIME_LIMIT', 30);
+define('BACKUP_DIR', false);
+define('USE_COMPOSER', true);
+define('COMPOSER_OPTIONS', '--no-dev');
+define('COMPOSER_HOME', false);
+define('EMAIL_ON_ERROR', false);
+
 ?>
